1dfe6fcd4603b7b172dd8d7d17b21d266cd89a73
Dylan McDowell <dylanjm@users.noreply.github.com>
2020-01-14 13:31:06 -0700

    add license and contributiion section

--- ox-jdf-report.el
+++ ox-jdf-report.el
@@ -11,15 +11,16 @@
 (add-to-list 'org-latex-classes
              '("jdf"
                "\\documentclass[letterpaper]{jdf}
-[NO-DEFAULT-PACKAGES]
-[PACKAGES]
-[EXTRA]"
+                [NO-DEFAULT-PACKAGES]
+                [PACKAGES]
+                [EXTRA]"
                ("\\section{%s}" . "\\section*{%s}")
                ("\\subsection{%s}" . "\\subsection*{%s}")
                ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                ("\\subsubsubsection{%s}" . "\\subsubsubsection*{%s}")
                ("\\paragraph{%s}" . "\\paragraph*{%s}")
                ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
 
 (org-export-define-derived-backend 'jdf 'latex
   :options-alist
@@ -36,41 +37,41 @@
                 (org-open-file (jdf-export-to-pdf nil s v b))))))))
 
 
-  (defun jdf-template (contents info)
-    "return complete document string for this export"
-    (concat
-     ;; Time-stamp.
-     (and (plist-get info :time-stamp-file)
-          (format-time-string "%% Created %Y-%m-%d %a %H:%M\n"))
-     ;; Document class and packages.
-     (let* ((class (plist-get info :latex-class))
-            (class-options (plist-get info :latex-class-options))
-            (header (nth 1 (assoc class org-latex-classes)))
-            (document-class-string
-             (and (stringp header)
-                  (if (not class-options) header
-                    (replace-regexp-in-string
-                     "^[ \t]*\\\\documentclass\\(\\(\\[[^]]*\\]\\)?\\)"
-                     class-options header t nil 1)))))
-       (if (not document-class-string)
-           (user-error "Unknown LaTeX class `%s'" class)
-         (org-latex-guess-babel-language
-          (org-latex-guess-inputenc
-           (org-element-normalize-string
-            (org-splice-latex-header
-             document-class-string
-             org-latex-default-packages-alist ; Defined in org.el.
-             org-latex-packages-alist nil     ; Defined in org.el.
-             (concat (org-element-normalize-string (plist-get info :latex-header))
-                     (plist-get info :latex-header-extra)))))
-          info)))
+(defun jdf-template (contents info)
+  "return complete document string for this export"
+  (concat
+   ;; Time-stamp.
+   (and (plist-get info :time-stamp-file)
+        (format-time-string "%% Created %Y-%m-%d %a %H:%M\n"))
+   ;; Document class and packages.
+   (let* ((class (plist-get info :latex-class))
+          (class-options (plist-get info :latex-class-options))
+          (header (nth 1 (assoc class org-latex-classes)))
+          (document-class-string
+           (and (stringp header)
+                (if (not class-options) header
+                  (replace-regexp-in-string
+                   "^[ \t]*\\\\documentclass\\(\\(\\[[^]]*\\]\\)?\\)"
+                   class-options header t nil 1)))))
+     (if (not document-class-string)
+         (user-error "Unknown LaTeX class `%s'" class)
+       (org-latex-guess-babel-language
+        (org-latex-guess-inputenc
+         (org-element-normalize-string
+          (org-splice-latex-header
+           document-class-string
+           org-latex-default-packages-alist ; Defined in org.el.
+           org-latex-packages-alist nil     ; Defined in org.el.
+           (concat (org-element-normalize-string (plist-get info :latex-header))
+                   (plist-get info :latex-header-extra)))))
+        info)))
 
-     ;; Now the core content
-     (let ((email (plist-get info :email))
-           (author (plist-get info :author))
-           (title (plist-get info :title))
-           (bib (plist-get info :bibfile)))
-       (concat "
+   ;; Now the core content
+   (let ((email (plist-get info :email))
+         (author (plist-get info :author))
+         (title (plist-get info :title))
+         (bib (plist-get info :bibfile)))
+     (concat "
 \\author{" (org-export-data author info) "}
 \\email{" (org-export-data email info) "}
 \\title{" (org-export-data title info) "}
@@ -169,6 +170,8 @@ Return PDF file's name."
 ;;;###autoload
 (defun jdf-export-to-pdf-and-open
     (&optional async subtreep visible-only body-only ext-plist)
+  "Export current buffer as a JDF report (pdf) and open the file in default
+PDF Viewer."
   (interactive)
   (let* ((*TEXINPUTS* (format "TEXINPUTS=%s%s/tex/latex/jdf:"
                               (or (getenv "TEXINPUTS") "")
